190d12fa0fa6829650325fba83be4abff389858f
Adam Lynch <contact@adamlynch.com>
2015-11-05 15:19:50 +0000

    Fix for browserify usage in test/shared_inner.js

--- test/shared_inner.js
+++ test/shared_inner.js
@@ -18,8 +18,8 @@ test('inner dep shared with multiple modules in external bundle', function (t) {
             externalBundle.bundle(function(err, extSrc) {
                 if (err) throw err;
 
-                t.ok(parentSrc.match(/INNER DEP/), "inner dep code must be in parent src");
-                t.notOk(extSrc.match(/INNER DEP/), "inner dep code must not be in external bundle");
+                t.ok(parentSrc.toString('utf8').match(/INNER DEP/), "inner dep code must be in parent src");
+                t.notOk(extSrc.toString('utf8').match(/INNER DEP/), "inner dep code must not be in external bundle");
 
                 var c = {
                     setTimeout: setTimeout,
